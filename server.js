7b07362aa58beecca28269da6f8e3c2ee72e5ab4
elia-dot <eliacohen100@gmail.com>
2023-04-05 07:46:43 +0300

    update log

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ const server = http.createServer((req, res) => {
 // start server
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running`);
 });
